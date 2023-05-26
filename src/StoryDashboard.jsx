fe195de3d98c82bc76146e9feb295d80dc7752c6
Andreas McDermott <andreas.mcdermott@shortcut.com>
2023-05-26 12:34:04 -0700

    Fixing.

--- src/StoryDashboard.jsx
+++ src/StoryDashboard.jsx
@@ -7,7 +7,8 @@ import { StoryName } from "./components/StoryName";
 import { StoryInlineActions } from "./components/StoryInlineActions";
 import { MiniKanban } from "./components/MiniKanban";
 import { SectionTitle } from "./components/SectionTitle";
-import { createEffect } from "solid-js";
+
+const orderOfStates = { backlog: 0, unstarted: 1, started: 2, done: 3 };
 
 export const StoryDashboard = () => {
   const activeStories = useActiveStories();
@@ -43,17 +44,35 @@ export const StoryDashboard = () => {
     }, {})
   );
 
+  const storiesByStateType = createMemo(() =>
+    (activeStories() || []).reduce(
+      (acc, story) => {
+        acc[orderOfStates[story.state.type]].push(story);
+        return acc;
+      },
+      [[], [], [], []]
+    )
+  );
+
   const [currentStory, setCurrentStory] = createSignal(null);
-  createEffect(() => {
-    if (currentStory()) {
+
+  const highlightStory = (storyId, scrollTo = false) => {
+    setCurrentStory(storyId);
+    if (scrollTo && storyId) {
       const storyEl = document.body.querySelector(
-        `[data-active-stories] [data-story-id="${currentStory()}"]`
+        `[data-active-stories] [data-story-id="${storyId}"]`
       );
-      if (storyEl) {
-        storyEl.scrollIntoView();
+      if (
+        storyEl &&
+        storyEl.previousElementSibling &&
+        storyEl.previousElementSibling.matches("[data-story-id]")
+      ) {
+        storyEl.previousElementSibling.scrollIntoView();
+      } else if (storyEl && storyEl.parentElement) {
+        storyEl.parentElement.scrollTo(0, 0);
       }
     }
-  });
+  };
 
   return (
     <div class="flex flex-column g2">
@@ -72,8 +91,8 @@ export const StoryDashboard = () => {
                       <StoryTypeBadge
                         type={story.story_type}
                         highlight={story.id === currentStory()}
-                        onMouseEnter={() => setCurrentStory(story.id)}
-                        onMouseLeave={() => setCurrentStory(null)}
+                        onMouseEnter={() => highlightStory(story.id, true)}
+                        onMouseLeave={() => highlightStory(null)}
                       />
                     )}
                   </For>
@@ -86,28 +105,41 @@ export const StoryDashboard = () => {
         <div class="pv1 bg-white br3 bb b--moon-gray">
           <ul
             data-active-stories
-            class="list pa0 ma0"
+            class="list pa0 ma0 flex flex-column"
             style="max-height: 140px; overflow-y: auto;"
           >
-            <For each={activeStories()}>
-              {(story) => (
-                <li
-                  data-story-id={story.id}
-                  class="f5 flex pa0 ma0 ph2 g2 items-center mw-100 relative overflow-hidden"
-                  onMouseEnter={() => {
-                    setCurrentStory(story.id);
-                  }}
-                  onMouseLeave={() => {
-                    setCurrentStory(null);
-                  }}
-                >
-                  <StoryTypeBadge
-                    type={story.story_type}
-                    highlight={story.id === currentStory()}
-                  />
-                  <StoryName>{story.name}</StoryName>
-                  <StoryInlineActions story={story} />
-                </li>
+            <For each={storiesByStateType().filter((s) => s.length)}>
+              {(stories) => (
+                <>
+                  <strong
+                    class="ph2 ttc bg-white"
+                    style="position: sticky; top: 0; z-index: 2; display: block;"
+                  >
+                    {stories[0].state.type}
+                  </strong>
+                  <For each={stories}>
+                    {(story) => (
+                      <li data-story-id={story.id}>
+                        <div
+                          class="f5 flex pa0 ma0 ph2 g2 items-center mw-100 relative overflow-hidden"
+                          onMouseEnter={() => {
+                            highlightStory(story.id);
+                          }}
+                          onMouseLeave={() => {
+                            highlightStory(null);
+                          }}
+                        >
+                          <StoryTypeBadge
+                            type={story.story_type}
+                            highlight={story.id === currentStory()}
+                          />
+                          <StoryName>{story.name}</StoryName>
+                          <StoryInlineActions story={story} />
+                        </div>
+                      </li>
+                    )}
+                  </For>
+                </>
               )}
             </For>
           </ul>
